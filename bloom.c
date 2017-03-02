4871affda21ba890fce3f648fd2918523bcccdfc
gregchalmers <g.chalmers@metocean.co.nz>
2017-03-02 14:05:32 +1300

    made makefile 1/2 OK

--- bloom.c
+++ bloom.c
@@ -25,77 +25,77 @@ static inline bool get_bit(uint8_t *array, int index) {
 static inline void str_to_lower(char *dest, const char *src) {
     while (*src) {
         *dest = tolower(*src);
-        dest++;        
+        dest++;
         src++;
     }
 }
 
-void bloom_add(uint8_t *bitmap, 
-               size_t bitmap_bits, 
-               uint8_t num_hashes,  
-               const uint8_t *data,
+void bloom_add(uint8_t *bitmap,
+               size_t bitmap_bits,
+               uint8_t num_hashes,
+               const char *data,
                size_t len) {
-    
-  uint128 hash = CityHash128(data, len);  
+
+  uint128 hash = CityHash128(data, len);
   for (int n = 0; n < num_hashes; n++)
       set_bit(bitmap , nth_hash(n, hash.first, hash.second, bitmap_bits));
 }
 
-void bloom_add_str_i(uint8_t *bitmap, 
-               size_t bitmap_size, 
-               uint8_t num_hashes,  
+void bloom_add_str_i(uint8_t *bitmap,
+               size_t bitmap_size,
+               uint8_t num_hashes,
                const char *str) {
-    
+
     int key_len = strlen(str);
-    char *key = alloca(key_len);
+    char *key = (char *)alloca(key_len);
     str_to_lower(key, str);
     bloom_add(bitmap, bitmap_size, num_hashes, key, key_len);
 }
 
-void bloom_add_str(uint8_t *bitmap, 
-               size_t bitmap_size, 
-               uint8_t num_hashes,  
+void bloom_add_str(uint8_t *bitmap,
+               size_t bitmap_size,
+               uint8_t num_hashes,
                const char *str) {
     bloom_add(bitmap, bitmap_size, num_hashes, str, strlen(str));
 }
 
-bool bloom_possibly_contains(uint8_t *bitmap, 
-                             size_t bitmap_bits, 
+bool bloom_possibly_contains(uint8_t *bitmap,
+                             size_t bitmap_bits,
                              uint8_t num_hashes,
-                             const uint8_t *data, 
+                             const char *data,
                              size_t len) {
-    
+
     uint128 hash = CityHash128(data, len);
     for (int n = 0; n < num_hashes; n++)
         if (!get_bit(bitmap, nth_hash(n, hash.first, hash.second, bitmap_bits)))
             return false;
     return true;
 }
-        
-bool bloom_possibly_contains_str_i(uint8_t *bitmap, 
-                                   size_t bitmap_bits, 
+
+bool bloom_possibly_contains_str_i(uint8_t *bitmap,
+                                   size_t bitmap_bits,
                                    uint8_t num_hashes,
                                    const char *str) {
 
     int key_len = strlen(str);
-    char *key = alloca(key_len);
+    char *key = (char *)alloca(key_len);
     str_to_lower(key, str);
-    
-    return bloom_possibly_contains(bitmap, 
+
+    return bloom_possibly_contains(bitmap,
                                    bitmap_bits,
                                    num_hashes,
                                    key,
                                    key_len);
 }
 
-bool bloom_possibly_contains_str(uint8_t *bitmap, 
-                                 size_t bitmap_bits, 
+bool bloom_possibly_contains_str(uint8_t *bitmap,
+                                 size_t bitmap_bits,
                                  uint8_t num_hashes,
                                  const char *str) {
 
-    return bloom_possibly_contains(bitmap, 
+    return bloom_possibly_contains(bitmap,
                                    bitmap_bits,
                                    num_hashes,
-                                   (uint8_t*)str,
+                                   str,
                                    strlen(str));
 }
--- bloom.h
+++ bloom.h
@@ -3,35 +3,35 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-void bloom_add(uint8_t *bitmap, 
-               size_t bitmap_bits, 
-               uint8_t num_hashes,  
-               const uint8_t *data,
+void bloom_add(uint8_t *bitmap,
+               size_t bitmap_bits,
+               uint8_t num_hashes,
+               const char *data,
                size_t len);
 
-void bloom_add_str(uint8_t *bitmap, 
-                   size_t bitmap_bits, 
-                   uint8_t num_hashes,  
+void bloom_add_str(uint8_t *bitmap,
+                   size_t bitmap_bits,
+                   uint8_t num_hashes,
                    const char *str);
 
-void bloom_add_str_i(uint8_t *bitmap, 
-                     size_t bitmap_bits, 
-                     uint8_t num_hashes,  
+void bloom_add_str_i(uint8_t *bitmap,
+                     size_t bitmap_bits,
+                     uint8_t num_hashes,
                      const char *str);
 
-bool bloom_possibly_contains(uint8_t *bitmap, 
-                             size_t bitmap_bits, 
+bool bloom_possibly_contains(uint8_t *bitmap,
+                             size_t bitmap_bits,
                              uint8_t num_hashes,
-                             const uint8_t *data, 
+                             const char *data,
                              size_t len);
-    
-bool bloom_possibly_contains_str(uint8_t *bitmap, 
-                                 size_t bitmap_bits, 
+
+bool bloom_possibly_contains_str(uint8_t *bitmap,
+                                 size_t bitmap_bits,
                                  uint8_t num_hashes,
                                  const char *str);
 
-bool bloom_possibly_contains_str_i(uint8_t *bitmap, 
-                                   size_t bitmap_bits, 
+bool bloom_possibly_contains_str_i(uint8_t *bitmap,
+                                   size_t bitmap_bits,
                                    uint8_t num_hashes,
                                    const char *str);
 
